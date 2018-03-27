05fb13ef618f4ba22463dafdc2f9a9f6ed5e9e28
rocheandley kwidama <rocheandley@gmail.com>
2018-03-27 19:00:13 +0200

    whatever

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,6 +41,6 @@ class LoginController extends Controller
 
 protected function credentials(Request $request)
     {
-        return ['email'=>$request->{$this->username()},'password' =>$request -> password,'status' => '1' ];
+        return ['email'=> $this->username(), 'password' =>$request->password, 'status' => '1' ];
     }
 }
